1d6a258072bb1459ff174b8089b503bf0afc9ee9
Hasnain <mkthassu72@gmail.com>
2026-07-08 10:01:24 +0530

    .

--- src/app.js
+++ src/app.js
@@ -36,9 +36,9 @@ app.use(
     }),
 );
 
-app.get("/", (req, res) => {
-    res.send("🚀 Backend is running successfully");
-});
+// app.get("/", (req, res) => {
+//     res.send("🚀 Backend is running successfully");
+// });
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true }));
