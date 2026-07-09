d2714393e3b4329a83027b110f7f305e2d23951d
Hasnain <mkthassu72@gmail.com>
2026-07-09 12:23:25 +0530

    email config fix

--- src/service/emailTransporter.js
+++ src/service/emailTransporter.js
@@ -1,10 +1,22 @@
 import nodemailer from "nodemailer";
 
 
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.gmail.com",
+//   port: 587,
+//   secure: false, 
+//   auth: {
+//     user: process.env.SMTP_USER,
+//     pass: process.env.SMTP_PASS,
+//   },
+// });
+
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
-  secure: false, 
+  secure: false,
+  requireTLS: true,
+  family: 4, // Force IPv4
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
