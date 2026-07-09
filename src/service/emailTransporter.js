1d0d3c7dedb2cd7ced3cb2a50d699b49f0e48a36
Hasnain <mkthassu72@gmail.com>
2026-07-09 12:44:24 +0530

    test

--- src/service/emailTransporter.js
+++ src/service/emailTransporter.js
@@ -13,16 +13,17 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  requireTLS: true,
-  family: 4, // Force IPv4
+  port: 465,
+  secure: true,
+  family: 4,
+  connectionTimeout: 1000,
+  greetingTimeout: 1000,
+  socketTimeout: 1000,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
 });
-
 transporter.verify((err, success) => {
   if (err) {
     console.log(err);
